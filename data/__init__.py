1e97e0442070b8c060e2dffab94044337ab96b17
Joona Särkijärvi <joona.sarkijarvi@gmail.com>
2020-10-14 11:01:50 +0300

    get_decade_heat_data: Method for gathering a decade worth of heating data.

--- data/__init__.py
+++ data/__init__.py
@@ -4,10 +4,10 @@
 try:
     from data.seasonal_anomalities import get_seasonal_anomalities
     from data.properties import get_properties
-    from data.energy import get_monthly_energy_data
+    from data.energy import get_monthly_energy_data, get_decade_heat_data
     from data.weather import get_avg_temperatures
 except ModuleNotFoundError:
     from .seasonal_anomalities import get_seasonal_anomalities
     from .properties import get_properties
-    from .energy import get_monthly_energy_data
+    from .energy import get_monthly_energy_data, get_decade_heat_data
     from .weather import get_decade_temperatures
--- data/energy.py
+++ data/energy.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import urllib.parse as urlparse
+from scipy import stats
 
 try:
     from lib import open_url, debug
@@ -28,3 +29,21 @@ def get_monthly_energy_data(buildingCode, reporting_group, start_time, end_time)
   search_url = f'{ENERGY_RESOURCE}?{params}'
   ret = pd.read_json(open_url(search_url))
   return ret
+
+def get_decade_heat_data(buildingCode):
+    """
+    Get 10 years of heat data for building.
+    throws: HTTPError if resource does not exist
+    """
+
+    data = get_monthly_energy_data(buildingCode, 'Heat', '2010-01-01', '2020-01-01')
+    data["date"] = pd.to_datetime(data["timestamp"])
+    #Set data for each month to be listed on first day of the month
+    data["date"] = pd.to_datetime(data["date"].apply(lambda x: x.strftime('%Y-%m')))
+    data.set_index("date", inplace=True)
+    data.drop(["timestamp", "reportingGroup", "locationName", "unit"], axis=1, inplace=True)
+    #Drop extreme outliers. We need might need to loop a few times, because some outliers are so big they make other outliers look normal 
+    while (np.abs(stats.zscore(data)) >= 6).any():
+        data = data[(np.abs(stats.zscore(data)) < 6).all(axis=1)]
+    return data
+
