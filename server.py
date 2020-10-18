8178d855df5b9ee8d80b0fe716eb0b67b754f611
Roger Blomgren <roger.blomgren@iki.fi>
2020-10-18 17:05:58 +0300

    Wire up route for energy-temperature graphics.

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask, Response
+from flask import Flask, Response, abort, send_file
 import re
 import pandas as pd
+import io
 
 import data
+import graphics
 from data.wrangler import get_data
 
 app = Flask(__name__)
 
 print('Reading properties ...')
 df_buildings = get_data('heated_buildings').or_fail()
+df_temperatures = get_data('decade_temperatures').or_fail()
 
 @app.route('/')
 def index():
@@ -23,6 +26,16 @@ def properties():
     return Response(
         df_buildings[['propertyName', 'latitude', 'longitude']].to_json(orient='index'),
         mimetype='application/json')
+
+@app.route('/api/properties/<building>/energy_history')
+def energy_history(building):
+    if not building in df_buildings.index:
+        abort(404)
+    # Store image in memory
+    buf = io.BytesIO()
+    graphics.plot_energy_temperature_history(df_buildings.loc[building], df_temperatures, buf)
+    buf.seek(0)
+    return send_file(buf, mimetype='image/png')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
