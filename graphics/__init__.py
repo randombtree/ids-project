7df3a72ccdb8fb8a0e82cc27ed614fe918a4d7ed
Roger Blomgren <roger.blomgren@iki.fi>
2020-10-20 18:50:21 +0300

    Make the plot readable.

--- graphics/__init__.py
+++ graphics/__init__.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
 
 from lib.util import month_range
 
@@ -18,8 +20,10 @@ def plot_energy_temperature_history(building, temp_df, prognosis_df, out):
     df = df.merge(right = prognosis_df, left_index = True, right_index = True, how = 'left')
     # The query results in some random column name for heat values, fix it:
     df.rename(columns={heat_data.name: 'value'}, inplace = True)
+
     # Change to datetime
-    df.rename(index = lambda s: pd.to_datetime(s))
+    df.rename(index = lambda s: datetime.datetime.fromisoformat(s), inplace = True)
+    print(type(df.index[0]))
     fig, ax = plt.subplots(figsize=(5,4))
     # Heating actualized
     ax.plot(df["value"], 'r-')
@@ -27,10 +31,18 @@ def plot_energy_temperature_history(building, temp_df, prognosis_df, out):
     ax.legend("Energy", loc="upper left")
     # Prognosis
     ax.plot(df.heating, 'r:')
+    # Set year formatting
+    ax.xaxis.set_major_locator(mdates.YearLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    ax.xaxis.set_minor_locator(mdates.MonthLocator())
+
     # Temperatures
     ax2 = ax.twinx()
     plt.gca().invert_yaxis()
     ax2.plot(df["avg_temp"], 'g-', alpha=0.7)
     ax2.set_ylabel("Average temperature, °C")
     ax2.legend("Temp (inv)")
+
+    plt.tight_layout()
+    fig.autofmt_xdate()
     plt.savefig(out)
