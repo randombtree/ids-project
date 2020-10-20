529c4d15104ac3eb8791d011ed54fbf258d8709b
Roger Blomgren <roger.blomgren@iki.fi>
2020-10-20 18:11:55 +0300

    Fill up date indexes when drawing.

--- graphics/__init__.py
+++ graphics/__init__.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 from lib.util import month_range
@@ -9,9 +10,16 @@ def plot_energy_temperature_history(building, temp_df, prognosis_df, out):
     """
     heat_data = building[list(month_range(building.heating_start, building.heating_stop))]
     df = pd.merge(heat_data, temp_df["avg_temp"], left_index=True, right_index=True)
-    df = pd.concat([df, prognosis_df])
+    # Now to fill the dots in between if data has holes in the end
+    for d in month_range(building.heating_stop, prognosis_df.index[-1]):
+        if not d in df.index:
+            df.loc[d] = np.nan
+    # Add prognosis
+    df = df.merge(right = prognosis_df, left_index = True, right_index = True, how = 'left')
     # The query results in some random column name for heat values, fix it:
     df.rename(columns={heat_data.name: 'value'}, inplace = True)
+    # Change to datetime
+    df.rename(index = lambda s: pd.to_datetime(s))
     fig, ax = plt.subplots(figsize=(5,4))
     # Heating actualized
     ax.plot(df["value"], 'r-')
